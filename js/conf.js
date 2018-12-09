17f9bfb984b9cd306d092871664dda8aa3ea9637
wong@Mac <zhandouxiaojiji@gmail.com>
2018-12-09 22:35:53 +0800

    sample

--- js/conf.js
+++
@@ -1,8 +0,0 @@
-layui.config({
-    base:'./js/modules/'
-});
-
-window.serverlist = [
-	{name:"test1", host:"http://huangjx.top:9999", desc:"测试1服"},
-	{name:"test2", host:"http://huangjx.top:9999", desc:"测试2服"}
-];
