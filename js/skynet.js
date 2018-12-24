b8171b735c38f060f04b7b90fae6ba82fe9dd198
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2018-12-24 20:53:50 +0800

    弹出层

--- js/skynet.js
+++ js/skynet.js
@@ -9,14 +9,14 @@ layui.use(['form', 'table', 'layer','jquery','session'], function(exports){
         let content = data.content;
         $('.childrenBody').append(content);
         function parse_action(action){
-            let id = action[0];
-            let a = action[1];
-            let element = $("#"+id)
+            let a = action[0];
+            let id = action[1];
             switch (a) {
                 case "ALERT":
-                    alert(action[2]);
+                    alert(action[1]);
                     break;
                 case "CLICK":
+                    var element = $("#"+id)
                     element.click(function(){
                         let args = action[2];
                         if (typeof(args[0]) == "object"){
@@ -30,40 +30,46 @@ layui.use(['form', 'table', 'layer','jquery','session'], function(exports){
                     });
                     break;
                 case "POST":
-                    element.click(function(){
-                        var param = {};
-                        for (let i in action[3]){
-                            let v = action[3][i];
-                            param[v[0]] = parse_action(v);
-                        }
-                        console.log(param);
-                        session.call(action[2], param, function(data){
-                            let cb = data.cb;
-                            if(typeof(cb[0])=="object"){
-                                for(let i in cb){
-                                    parse_action(cb[i]);
-                                }
-                            }else{
-                                parse_action(cb);
+                    var param = {};
+                    for (let i in action[2]){
+                        let v = action[2][i];
+                        param[v[1]] = parse_action(v);
+                    }
+                    console.log(param);
+                    session.call(action[1], param, function(data){
+                        let cb = data.cb;
+                        if(typeof(cb[0])=="object"){
+                            for(let i in cb){
+                                parse_action(cb[i]);
                             }
-                        });
-                        return false;
+                        }else{
+                            parse_action(cb);
+                        }
                     });
-                    break;
+                    return false;
                 case "GET_VAL":
+                    var element = $("#"+id);
+                    console.log("&&&&&& GET_VAL", id, element, element.val());
                     return element.val();
                 case "SET_VAL":
+                    var element = $("#"+id);
                     element.val(action[2]);
                     break;
                 case "APPEND_VAL":
+                    var element = $("#"+id);
                     element.val(element.val()+action[2]+'\n');
                     break;
                 case "GET_TEXT":
+                    var element = $("#"+id);
                     return element.text();
                 case "SET_TEXT":
+                    var element = $("#"+id);
                     element.text(action[2]);
                     break;
                 case "SUBMIT":
+                    break;
+                case "OPEN":
+                    layer.open(action[1]);
                     break;
             }
         }
@@ -75,7 +81,19 @@ layui.use(['form', 'table', 'layer','jquery','session'], function(exports){
             table.render(data.tables[i]);
         }
         for(let i in data.actions){
-            parse_action(data.actions[i])
+            parse_action(data.actions[i]);
         }
+        if(data.table_on){
+            for(let i in data.table_on){
+                let v = data.table_on[i];
+                table.on(v[0], function(obj) {
+                    let action = v[1][obj.event];
+                    if(action){
+                        parse_action(action);
+                    }
+                })
+            }
+        }
+       
     })
 });
